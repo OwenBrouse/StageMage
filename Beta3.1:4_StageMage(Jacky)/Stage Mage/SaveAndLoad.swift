ef28602fa82be4231eb3691c6b8d8b67259f8c10
Jacky <jlian5@ocdsb.ca>
2019-01-16 13:27:19 -0500

    added new versions (16/1/2019)

---
+++ Beta3.1:4_StageMage(Jacky)/Stage Mage/SaveAndLoad.swift
@@ -0,0 +1,16 @@
+//
+//  SaveAndLoad.swift
+//  Stage Mage
+//
+//  Created by Jacky Liang on 2018-12-19.
+//  Copyright © 2018 KingByngInc. All rights reserved.
+//
+
+import UIKit
+
+class OpenProjects: UIViewController, UIScrollViewDelegate {
+ 
+     @objc override func viewDidLoad() {
+        
+    }
+}
