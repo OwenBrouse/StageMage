a58cac41b051d48fab2a6fbcbf7a8dd3536b7b53
Owen Brouse <33287430+OwenBrouse@users.noreply.github.com>
2019-01-16 13:27:12 -0500

    MERGED part1

---
+++ Stage Mage/HelpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HelpViewController.swift
+//  Stage Mage
+//
+//  Created by Jacky Liang on 2019-01-15.
+//  Copyright © 2019 KingByngInc. All rights reserved.
+//
+
+import UIKit
+
+class HelpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
