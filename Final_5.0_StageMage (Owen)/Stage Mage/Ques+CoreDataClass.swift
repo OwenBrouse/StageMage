fbbd755beeaa50eb8f459845d140ea81bdbb41ce
Jacky <jlian5@ocdsb.ca>
2019-01-19 15:56:16 -0500

    rename folder and add 4.0 - 5.0 (19/1/2019)

---
+++ Final_5.0_StageMage (Owen)/Stage Mage/Ques+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Ques+CoreDataClass.swift
+//  Stage Mage
+//
+//  Created by Jacky Liang on 2019-01-18.
+//  Copyright © 2019 KingByngInc. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Ques)
+public class Ques: NSManagedObject {
+
+}
